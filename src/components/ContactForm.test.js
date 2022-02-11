bf731ae7fb6fc01fd78cfd5e80cf5b71ad325b95
Schuyler <skym1n0r91@gmail.com>
2022-02-10 18:12:23 -0600

    Email error renders by itself

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -27,9 +27,31 @@ test("renders ONE error message if user enters less then 5 characters into first
   expect(errorMessage).toHaveLength(1);
 });
 
-test("renders THREE error messages if user enters no values into any fields.", async () => {});
+test("renders THREE error messages if user enters no values into any fields.", async () => {
+  render(<ContactForm />);
+  const submitButton = screen.getByRole("button");
+  userEvent.click(submitButton);
 
-test("renders ONE error message if user enters a valid first name and last name but no email.", async () => {});
+  await waitFor(() => {
+    const errorMessage = screen.queryAllByTestId("error");
+    expect(errorMessage).toHaveLength(3);
+  });
+});
+
+test("renders ONE error message if user enters a valid first name and last name but no email.", async () => {
+  render(<ContactForm />);
+  const firstNameField = screen.getByLabelText(/first name*/i);
+  userEvent.type(firstNameField, "schuyler");
+
+  const lastNameField = screen.getByLabelText(/last name*/i);
+  userEvent.type(lastNameField, "Minor");
+
+  const button = screen.getByRole("button");
+  userEvent.click(button);
+
+  const errorMessage = await screen.getAllByTestId("error");
+  expect(errorMessage).toHaveLength(1);
+});
 
 test('renders "email must be a valid email address" if an invalid email is entered', async () => {});
 
